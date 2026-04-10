b149f8fd689d72627ff80432c5875d04e6070c26
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2026-04-10 23:33:13 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from pydub import AudioSegment
+
+input_folder = 'D:/Tools/ConvertToOGG/input'
+output_folder = 'D:/Tools/ConvertToOGG/output'
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(input_folder):
+    if filename.endswith(('.mp3', '.wav', '.flac', '.aac', '.m4a')):
+        file_path = os.path.join(input_folder, filename)
+        audio = AudioSegment.from_file(file_path)
+
+        audio = audio.set_frame_rate(48000).set_channels(1)
+
+        output_file_path = os.path.join(output_folder, f'{os.path.splitext(filename)[0]}.ogg')
+        audio.export(output_file_path, format='ogg')
+
+        print(f'변환 완료 {output_file_path}')
